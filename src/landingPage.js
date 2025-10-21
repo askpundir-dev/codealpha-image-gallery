94f90d318a226e245816ef5fd2975fcfad14d832
Abhishek Pundir <ask.pundir0224@gmail.com>
2025-10-21 18:17:00 +0530

    add gallery images

--- src/landingPage.js
+++ src/landingPage.js
@@ -69,7 +69,7 @@ setInterval(() => {
   images[currentIndex].style.opacity = 0;
   currentIndex = (currentIndex + 1) % images.length;
   images[currentIndex].style.opacity = 1;
-}, 4500);
+}, 3800);
 
 const enterGalleryButton = document.querySelector(".enter-gallery");
 enterGalleryButton.onclick = () => (window.location.href = "gallery.html");
